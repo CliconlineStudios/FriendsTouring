1d3f514195bb5e08b6c1be7cdaaa28b32d566462
CliconlineStudios <105317102+CliconlineStudios@users.noreply.github.com>
2023-01-30 22:01:21 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -39,24 +39,29 @@ function backweb() {
         document.getElementById("informacion").style.display = "block";
         document.getElementById("portafolio").style.display = "none";
         document.getElementById("side").style.display = "none";
+        document.getElementById("parejas").style.display = "none";
+        document.getElementById("family").style.display = "none";
+        document.getElementById("aventura").style.display = "none";
         document.getElementById("screen4").style.display = "none";
         document.getElementById("screen5").style.display = "none";
         conta=0;
         contador=0;
         backarriba();
         history.back();
-        offcards();
+     
 
     }
     else if(contador==2){
         document.getElementById("portafolio").style.display = "block";
-        document.getElementById("viajes").style.display = "none";
+        document.getElementById("parejas").style.display = "none";
+        document.getElementById("family").style.display = "none";
+        document.getElementById("aventura").style.display = "none";
         document.getElementById("screen4").style.display = "none";
         document.getElementById("screen5").style.display = "none";
         contador=1;
         backarriba();
         ids1();
-        offcards();
+     
 
     }
     else{
@@ -66,31 +71,19 @@ function backweb() {
 
     } 
 
-function offcards(){
-    document.getElementById("quito").style.display = "block";
-    document.getElementById("imindo").src=  `https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgbdGXOv0PxJFbBK_2KS6fIw236Ixaf9WBgRe2FRPkeblMjrGI43Q-glPM3GXiU2FMkDX6uHgCN7HHXKeiXeTvuBu777-2UEclWpWsCwTHQfbLIk2r1-i9saj-9Ry5GwV2RgE28BW23QR92htYbCb4MamWfshYpcyHr0K5QhzR7FNqnoTc71P3RlNwQ/s320/minddo.png`;
-    document.getElementById("imggalapagos2").src= `https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjBdbn9jKn77wQlLUdk1VtoYAMumuc7Ko-_S8Fh57825UCGJBz07uR3UsJF7bWnXHV-YS9RH_GnkekyEKK0GJwSCytYOg4pMMzuaZk46poWpucx-drrA32yTc7FdrN6381Mo0kqEhYjf4fjWWWvPjDOd98Ugduc19bYbwowsUKMnsCF06KPxcnxxWhI/s320/mujeryfoca.jpg`;
-    document.getElementById("imggalapagos").src= `https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg9UfTf_32V32XJu1tPU7wsvBFzXu47zivHkBrERsAu9sNtwaZ6VJ0EG0CEW22M4eB2lWMmK3DvLWdorv-ZYstNLUGKEFOBr4cSKGOLprKL7YXQdXU9j1UtFvukhpUX3pimOIArH9RLWdAntQ8Dj7QlvzTWt09RC9Y0s6FJ9MEtm82zdLy0zMhJ5X8v/s320/galapagosfoca.jpg`;
-    document.getElementById("imgamazonas").src= `https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhHRpX0l5Oq0bGJIoyG3dlIKvUpU_p8-FbZD2PGIx9C---vIvQFuYw4CmXLUPu0oiOMavhhX1RwJTHflTOsc169fMoFRPyHdh7cHezVjLyHbGYUbssVNSu2t2Wfm6NIKLna1_DBpitKVJWNQLIyM6ti2LWz4goRzJ-4YI2K8m_Z5ji1k7YFXyPk3tn_/s320/amazonaslog.jpg`;
-    document.getElementById("galapagos2").style.display = "none";
-    document.getElementById("cotopaxi").style.display = "none";
-    document.getElementById("screen4").style.display = "none";
-};
-
-
 
 function inicio() {
     document.getElementById("informacion").style.display = "block";	
     document.getElementById("portafolio").style.display = "none";
-    document.getElementById("viajes").style.display = "none";
-    document.getElementById("side").style.display = "none";
+    document.getElementById("parejas").style.display = "none";
+    document.getElementById("family").style.display = "none";
+    document.getElementById("aventura").style.display = "none";
     document.getElementById("screen4").style.display = "none";
     document.getElementById("screen5").style.display = "none";
     conta=0;
     contador=0;
     backarriba();
     backweb();
-    offcards();
 
 }
 
@@ -99,12 +92,13 @@ function servicios() {
     document.getElementById("informacion").style.display = "none";
     document.getElementById("side").style.display = "none";
     document.getElementById("viajes").style.display = "none";
+    document.getElementById("aventura").style.display = "none";
     document.getElementById("screen4").style.display = "none";
     document.getElementById("screen5").style.display = "none";
     conta=0;
     contador=1;
     ids1();
-    offcards();
+
 
 };
 
@@ -114,6 +108,7 @@ function destinos() {
     document.getElementById("viajes").style.display = "block";
     document.getElementById("side").style.display = "none";
     document.getElementById("informacion").style.display = "none";
+    document.getElementById("aventura").style.display = "none";
     document.getElementById("destinos").textContent=  `Destinations`;
     document.getElementById("screen4").style.display = "none";
     document.getElementById("screen5").style.display = "none";
@@ -125,15 +120,11 @@ function destinos() {
 }
 
 function couples() {
-    document.getElementById("viajes").style.display = "block";
-    document.getElementById("galapagos").style.display = "block";
+    document.getElementById("parejas").style.display = "block";
     document.getElementById("informacion").style.display = "none";	
     document.getElementById("portafolio").style.display = "none";
-    document.getElementById("side").style.display = "none";
-    document.getElementById("destinos").textContent=  `Destinations in Couple`;
-    document.getElementById("screen4").style.display = "none";
-    document.getElementById("screen5").style.display = "none";
-    conta=0;
+    document.getElementById("viajes").style.display = "none";
+    document.getElementById("aventura").style.display = "none";
     contador=2;
     ids2();
     backarriba();
@@ -142,55 +133,28 @@ function couples() {
 
 
 
-function Family() {
-    document.getElementById("viajes").style.display = "block";
+function family() {
+    document.getElementById("family").style.display = "block";
+    document.getElementById("parejas").style.display = "none";
     document.getElementById("informacion").style.display = "none";	
     document.getElementById("portafolio").style.display = "none";
-    document.getElementById("galapagos").style.display = "none";
-    document.getElementById("galapagos2").style.display = "block";
-    document.getElementById("side").style.display = "none";
-    document.getElementById("destinos").textContent=  `Destinations in Family`;
-    document.getElementById("screen4").style.display = "none";
-    document.getElementById("screen5").style.display = "none";
+    document.getElementById("viajes").style.display = "none";
+    document.getElementById("aventura").style.display = "none";
     conta=0;
     contador=2;
     ids2();
     backarriba(); 
 }
 
-function Water() {
-    document.getElementById("viajes").style.display = "block";
-    document.getElementById("informacion").style.display = "none";	
-    document.getElementById("portafolio").style.display = "none";
-    document.getElementById("side").style.display = "none";
-    document.getElementById("quito").style.display = "none";
-    document.getElementById("destinos").textContent=  `Destinations in Water`;
-    document.getElementById("imgamazonas").src= `https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgaZVinLBxxK9_e9IWrZp_EKxJ9g12aBToUlQiy3TtQx4Tybnk1ly14UrDRw-FIjOunkorFWX33HbNpMoLdWZ4cwbZoYFQUHXfYIdasYoaCWuUG5FqqJ6iMw451c2DGxJ_RUpjgB5QIqzGtO8mJahc_KKxWAIL9RE0LF_MtO53a3Bt3LrY9-6dcBxIO/s320/amazonas225.jpg`;
-    document.getElementById("imggalapagos").src= `https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhvZrW_vPGQYN_WqD2pCUKKjZQ5QtWh3p75FZ7U0A0JLWvO4OxKJMdr1O9y8slhpuwpgMbl2yAZTDPo9PPSFZ1KU9OWxAU3eAX-F_4MCV799vLz-NNQQE2RGSIYpKG8z4ZcWhZrDsBBw0e3BKRmhvlEAzyAFDCsHq1uozKVdHW2_wsxJWK_AZWHtXVY/s320/galapagosagua.jpg`;
-    document.getElementById("imindo").src= `https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiLmHyA3ZUFAZq4enYtWtB_YRS0j85elGJq9zxm_5bTPZTNPf3S4yyX4YUDnb-dRDu4XTjTRvSQw5MX9DkD0YNzHl6DL0yVGvGHUO_YCOo5aNVGlow-xpfFN9yIZrB0P_HAD2ZF1lsGTBoCvQDaU2s25c5X7G-JMvv4Gtb1kjK5tTClzoJZ2nMVLwJ-/s320/mindocascada.jpg`;
-    document.getElementById("galapagos").style.display = "block";
-    document.getElementById("screen4").style.display = "none";
-    document.getElementById("screen5").style.display = "none";
-    conta=0;
-    contador=2;
-    ids2();
-    backarriba();    
-        
-}
 
 
 function aventura() {
+    document.getElementById("aventura").style.display = "block";
+    document.getElementById("family").style.display = "none";
+    document.getElementById("parejas").style.display = "none";
     document.getElementById("informacion").style.display = "none";	
     document.getElementById("portafolio").style.display = "none";
-    document.getElementById("viajes").style.display = "block";
-    document.getElementById("side").style.display = "none";
-    document.getElementById("informacion").style.display = "none";
-    document.getElementById("quito").style.display = "none";
-    document.getElementById("destinos").textContent=  `Destinations of Adventure`;
-    document.getElementById("galapagos2").style.display = "block";
-    document.getElementById("cotopaxi").style.display = "block";
-    document.getElementById("screen4").style.display = "none";
-    document.getElementById("screen5").style.display = "none";
+    document.getElementById("viajes").style.display = "none";
     conta=0;
     contador=2;
     ids2();
@@ -199,15 +163,16 @@ function aventura() {
 }
 
 function screnn4() {
+    document.getElementById("screen4").style.display = "block";
+    document.getElementById("informacion").style.display = "none";	
+    document.getElementById("portafolio").style.display = "none";
+    document.getElementById("screen5").style.display = "none";
+    document.getElementById("aventura").style.display = "none";
+    document.getElementById("family").style.display = "none";
+    document.getElementById("parejas").style.display = "none";
     document.getElementById("informacion").style.display = "none";	
     document.getElementById("portafolio").style.display = "none";
     document.getElementById("viajes").style.display = "none";
-    document.getElementById("side").style.display = "none";
-    document.getElementById("informacion").style.display = "none";
-    document.getElementById("quito").style.display = "none";
-    document.getElementById("cotopaxi").style.display = "block";
-    document.getElementById("screen4").style.display = "block";
-    document.getElementById("screen5").style.display = "none";
     conta=0;
     contador=2;
     ids2();
@@ -237,24 +202,34 @@ function screen5() {
 
 function back() {
 
+  
     if (contador==1) {
         document.getElementById("informacion").style.display = "block";
         document.getElementById("portafolio").style.display = "none";
         document.getElementById("side").style.display = "none";
+        document.getElementById("parejas").style.display = "none";
+        document.getElementById("family").style.display = "none";
+        document.getElementById("aventura").style.display = "none";
+        document.getElementById("screen4").style.display = "none";
+        document.getElementById("screen5").style.display = "none";
         conta=0;
         contador=0;
         backarriba();
         history.back();
-        offcards();
+     
 
     }
     else if(contador==2){
         document.getElementById("portafolio").style.display = "block";
-        document.getElementById("viajes").style.display = "none";
+        document.getElementById("parejas").style.display = "none";
+        document.getElementById("family").style.display = "none";
+        document.getElementById("aventura").style.display = "none";
+        document.getElementById("screen4").style.display = "none";
+        document.getElementById("screen5").style.display = "none";
         contador=1;
         backarriba();
         ids1();
-        offcards();
+     
 
     }
     else{
